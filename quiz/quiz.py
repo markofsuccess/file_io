d2809bd7ea1dbae615470f7eaebed3f47d962aac
Ubuntu <ubuntu@ip-172-31-91-180.ec2.internal>
2019-10-16 13:24:29 +0000

    added new quiz folder and quiz.py file with functions for quiz

---
+++ quiz/quiz.py
@@ -0,0 +1,23 @@
+def show_menu():
+    print("1. Ask questions")
+    print("2. Add a question")
+    print ("3. Exit game")
+    
+    option = input("Enter option: ")
+    return option
+    
+def game_loop():
+    while True:
+        option = show_menu()
+        if option == "1":
+            print("You selected 'Ask questions'")
+        elif option == "2":
+            print("You selected 'Add a question'")
+        elif option == "3":
+            break
+        else:
+            print("Invalid option")
+            print("")
+
+game_loop()
+        
